b2d3231f50b130695e2fc6c6a3e7d15c788904f8
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-09-07 10:19:36 -0300

    Filtros para busca filtrada ok

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlass } from "phosphor-react";
+import { FileSearch, MagnifyingGlass } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { Container, ContainerRepos, ContainerSearch, ContentRepos } from "./styles";
 
@@ -28,9 +28,21 @@ export function Main() {
     const filterSearch = search.length > 0 ? repos.filter(repo => repo.name.toLowerCase().includes(lowerSearch)) : [];
 
     order === 'name' ?
-    filterSearch.sort((a, b) => { return a.name.localeCompare(b.name) }) && repos.sort((a, b) => { return a.name.localeCompare(b.name) }) :
-    filterSearch.sort((a, b) => { return b.pushed_at.localeCompare(a.pushed_at) }) && repos.sort((a, b) => { return b.pushed_at.localeCompare(a.pushed_at) });
- 
+        filterSearch.sort((a, b) => { return a.name.localeCompare(b.name) }) && repos.sort((a, b) => { return a.name.localeCompare(b.name) }) :
+        filterSearch.sort((a, b) => { return b.pushed_at.localeCompare(a.pushed_at) }) && repos.sort((a, b) => { return b.pushed_at.localeCompare(a.pushed_at) });
+
+    switch (filterType) {
+        case 'forks': repos.filter(repo => repo.fork > 0 ? console.log(repo) : '');
+        break
+
+        case 'archived': repos.filter(repo => repo.archived === true ? console.log(repo) : '');
+        break
+
+        case 'license': repos.filter(repo => repo.license = true ? console.log(repo) : '');
+        break      
+    }
+    
+
     return (
         <Container>
             <ContainerSearch>
@@ -47,8 +59,9 @@ export function Main() {
                     <button onClick={e => setOrder('date')}>Data</button>
 
                     <select name="filtro" id="filtro" onChange={e => setFilterType(e.target.value)} >
-                        <option value="proprios" >Próprios</option>
-                        <option value="arquivados" >Arquivados</option>
+                        <option>Filtrar</option>
+                        <option value="archived" >Arquivados</option>
+                        <option value="license" >Com licença</option>
                         <option value="forks" >Forks</option>
                     </select>
 
